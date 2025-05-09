09606d2a68269da07ab69fc51c32618bdd8e0120
Alexander Zyubin <158208274+Alexanderzubin@users.noreply.github.com>
2025-05-09 20:49:34 +0300

    Исправил ошибку импорта

--- app/database/models/user.py
+++ app/database/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, BigInteger
 from sqlalchemy.orm import relationship
 from app.database.base import Base
 
